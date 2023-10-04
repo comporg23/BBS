65b9debbcb6b8b2f73634b3ce1ed36db311d4e2e
Norayr Chilingarian <norayr@arnet.am>
2023-10-04 14:02:25 +0400

    gets

---
+++ notes/stack/7/gets/arr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int arr[ 2 ];
+   int arr2[ 3000 ];
+
+   printf ("val = %d\n",  arr[ 1024 ]);
+
+   return 0;
+}
---
+++ notes/stack/7/gets/arr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int arr[ 2 ];
+
+   cout << arr[ 4 ] << endl;
+
+   return 0;
+}
